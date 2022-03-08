ad7a730340b805582a7edbcbd4c732682ec51b0c
Patrick <jpatrickpajarillo@gmail.com>
2022-03-07 23:59:45 -0800

    added Admin List Feature

--- src/pages/AdminPage/AdminPageElements.js
+++ src/pages/AdminPage/AdminPageElements.js
@@ -3,8 +3,29 @@ import styled from "styled-components";
 export const AdminPageContainer = styled.section`
     width: 100%;
     margin-top: 70px;
-    background: yellow;
     display: flex;
     justify-content: center;
     align-items: center;
+`;
+
+export const AdminPageWrapper = styled.div`
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 15px;
+`;
+
+export const AdminPageHeading = styled.h1`
+    font-size: 1.8rem;
+    margin-bottom: 25px;
+`;
+
+export const AdminPageUl = styled.ul`
+    list-style: none;
+`; 
+
+export const AdminPageLi = styled.li`
+    font-size: 1.3rem;
+    letter-spacing: 0.06rem;
+    color: #000;
 `;
--- src/pages/AdminPage/index.js
+++ src/pages/AdminPage/index.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import {AdminPageContainer
 
+import React, { useState, useEffect } from 'react';
+import {AdminPageContainer,
+        AdminPageWrapper,
+        AdminPageHeading,
+        AdminPageUl,
+        AdminPageLi
         } from "./AdminPageElements";
+import axios from "axios";
+
 
 const AdminPage = () => {
+
+  const [admins, setAdmins] = useState([]);
+
+  useEffect(() => {
+
+    axios.get('http://localhost:5000/adminList')
+        .then(res => {
+          console.log(res.data)
+          setAdmins(res.data)
+      })
+        .catch(err => console.log(err))
+
+  }, [])
+
   return (
     <AdminPageContainer>
-        
+        <AdminPageWrapper>
+          <AdminPageHeading>Administrators</AdminPageHeading>
+            <AdminPageUl>
+              {admins.filter((val) => {
+
+                if (val.role === 'admin') {
+                  return val;
+                } else {
+                  return null;
+                }
+
+              }).map((val, key) => {
+                return <AdminPageLi>{val.fullname}</AdminPageLi>
+              })}
+            </AdminPageUl>
+        </AdminPageWrapper>
     </AdminPageContainer>
   )
 }
